939cf9ae66d4b2ecdd273f6cf4a42bf79f88b6b0
Cleslley de Moura <100368699+cleslleydemoura@users.noreply.github.com>
2024-06-05 19:17:35 -0300

    Código SQL | Java MySQL

---
+++ aulateste/cod_sql.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE teste;
+
+CREATE TABLE teste.aluno (
+	login varchar (11),
+	senha varchar (10)
+);
+
+
+INSERT INTO teste.aluno (login, senha) VALUES ('cleslley', '8800');
+INSERT INTO teste.aluno (login, senha) VALUES ('victor', '4400');
+INSERT INTO teste.aluno (login, senha) VALUES ('cesar', '2200');
